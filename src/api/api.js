75b2dad7ea53ff2ee86beff1284dfbf47f59b81c
Malek Jehani <malek.r.j@hotmail.com>
2025-12-05 08:43:14 -0300

    test

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../auth/Autenticacao";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", 
+  baseURL: process.env.REACT_APP_ENDERECO_API,
 });
 
 api.interceptors.request.use((config) => {
